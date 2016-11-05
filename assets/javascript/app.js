50ef915a4fa0ba8c3157c2cf23d03fe5e4b5aa93
Josh Bruggeman <joshrbruggeman@gmail.com>
2016-11-05 13:31:40 -0500

    API KEY

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
-var
-var
-var
+ 
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "0294899d32474037bd7a7e530f01340f"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
+
