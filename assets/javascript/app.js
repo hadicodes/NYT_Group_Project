92508fd7a3e97fcb55f70926fd5aa7a5c8391192
Mark Gildea <gildeamark1@gmail.com>
2016-11-05 13:18:12 -0500

    initial files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,11 @@
-var
-var
-var
+$.ajax({
+		url: query URL,
+		method: 'GET'
+	})
+	.done(function(resp) {
+		var results = resp.data;
+
+		for (var i = 0; i < results.length; i++) {
+			results[i]
+		}
+	})
